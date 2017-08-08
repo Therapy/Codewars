769fb4152de8c9f8b0f1528f6cd45fc5f37fd9ce
Therapy <therapyspb@yandex.ru>
2017-08-08 20:50:00 +0300

    day 1 finished

--- 002-7kyu-get-the-middle-character.js
+++ 002-7kyu-get-the-middle-character.js
@@ -1,7 +1,11 @@
 //jshint esversion: 6
 //jscs:disable maximumLineLength
 
-// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+/*
+You are going to be given a word. Your job is to return the middle character of the word.
+If the word's length is odd, return the middle character. If the word's length is even,
+return the middle 2 characters.
+*/
 
 // const getMiddle = s => s.substr(s.length - 1 >>> 1, (~s.length & 1) + 1);
 
